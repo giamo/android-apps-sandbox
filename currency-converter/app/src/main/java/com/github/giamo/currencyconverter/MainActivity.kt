e83127739cc28ddc9b1e0180a6f053ac20afabcf
Gianluca Amori <gianluca.amori@gmail.com>
2020-03-28 10:29:25 +1300

    [currency-converter] Initial app structure with simple euro-dollar conversion

---
+++ currency-converter/app/src/main/java/com/github/giamo/currencyconverter/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.github.giamo.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val EUR_USD_RATIO = 1.1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun convert(view: View) {
+        val amountToConvertTextEUR = currencyEditText.text.toString()
+        if (amountToConvertTextEUR.isEmpty()) {
+            Toast.makeText(this, "An amount of money should be specified", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            val amountToConvertEUR = amountToConvertTextEUR.toDouble()
+            val convertedAmountUSD = amountToConvertEUR * EUR_USD_RATIO
+
+            Toast.makeText(
+                this,
+                "EUR $amountToConvertEUR = USD $convertedAmountUSD",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
+
