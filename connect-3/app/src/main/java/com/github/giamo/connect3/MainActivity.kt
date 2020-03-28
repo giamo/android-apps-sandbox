57d91bb30a3930613a97eae2dfde8da81cb9e2cf
Gianluca Amori <gianluca.amori@gmail.com>
2020-03-28 17:27:12 +1300

    [connect-3] Make tokens stick on drop and update grid representation

--- connect-3/app/src/main/java/com/github/giamo/connect3/MainActivity.kt
+++ connect-3/app/src/main/java/com/github/giamo/connect3/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.github.giamo.connect3
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private var nextMoveIsRed = true;
+    companion object {
+        const val EMPTY = 0
+        const val RED = 1
+        const val YELLOW = 2
+    }
+
+    private val board: Array<Array<Int>> =
+        Array<Array<Int>>(3) { _ -> arrayOf(EMPTY, EMPTY, EMPTY) }
+    private var currentPlayer = RED;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +27,50 @@ class MainActivity : AppCompatActivity() {
 
     fun dropToken(view: View) {
         val imageView = view as ImageView
-        imageView.translationY = -1000f
 
-        if (nextMoveIsRed) imageView.setImageResource(R.drawable.red)
-        else imageView.setImageResource(R.drawable.yellow)
+        val position = retrievePosition(imageView.tag?.toString())
+        if (position == null) {
+            Toast.makeText(this, "An error has occurred", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-        imageView.animate().translationYBy(1000f).setDuration(1000)
-        nextMoveIsRed = !nextMoveIsRed
+        if (isBoardCellEmpty(position)) {
+            updateBoard(position)
+            imageView.translationY = -1000f
+            currentPlayer = if (currentPlayer == RED) {
+                imageView.setImageResource(R.drawable.red)
+                YELLOW
+            } else {
+                imageView.setImageResource(R.drawable.yellow)
+                RED
+            }
+
+            imageView.animate().translationYBy(1000f).setDuration(1000)
+        }
+    }
+
+    private fun retrievePosition(tag: String?): Pair<Int, Int>? {
+        return tag.let {
+            when (it) {
+                "00" -> Pair(0, 0)
+                "01" -> Pair(0, 1)
+                "02" -> Pair(0, 2)
+                "10" -> Pair(1, 0)
+                "11" -> Pair(1, 1)
+                "12" -> Pair(1, 2)
+                "20" -> Pair(2, 0)
+                "21" -> Pair(2, 1)
+                "22" -> Pair(2, 2)
+                else -> null
+            }
+        }
+    }
+
+    private fun updateBoard(position: Pair<Int, Int>) {
+        if (isBoardCellEmpty(position)) board[position.first][position.second] = currentPlayer
+    }
+
+    private fun isBoardCellEmpty(position: Pair<Int, Int>): Boolean {
+        return (board[position.first][position.second] == EMPTY)
     }
 }
