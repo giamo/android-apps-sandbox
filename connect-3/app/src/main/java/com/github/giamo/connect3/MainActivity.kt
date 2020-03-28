5eedab431a02f7dc444c7018c5887bd408acf5b1
Gianluca Amori <gianluca.amori@gmail.com>
2020-03-28 16:45:20 +1300

    [connect-3] Make alternating color tokens drop on grid cell click

--- connect-3/app/src/main/java/com/github/giamo/connect3/MainActivity.kt
+++ connect-3/app/src/main/java/com/github/giamo/connect3/MainActivity.kt
@@ -2,11 +2,26 @@ package com.github.giamo.connect3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private var nextMoveIsRed = true;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun dropToken(view: View) {
+        val imageView = view as ImageView
+        imageView.translationY = -1000f
+
+        if (nextMoveIsRed) imageView.setImageResource(R.drawable.red)
+        else imageView.setImageResource(R.drawable.yellow)
+
+        imageView.animate().translationYBy(1000f).setDuration(1000)
+        nextMoveIsRed = !nextMoveIsRed
     }
 }
